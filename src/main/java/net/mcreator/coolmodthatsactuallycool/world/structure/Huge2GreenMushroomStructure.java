abf095f912d18b39665a832242f46de0fad87dfe
CactusHamster <gyrlg@DESKTOPATHOME>
2020-09-15 11:16:56 -0700

    3 Huge Green Mushroom Types Added

---
+++ src/main/java/net/mcreator/coolmodthatsactuallycool/world/structure/Huge2GreenMushroomStructure.java
@@ -0,0 +1,88 @@
+
+package net.mcreator.coolmodthatsactuallycool.world.structure;
+
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.placement.IPlacementConfig;
+import net.minecraft.world.gen.feature.template.Template;
+import net.minecraft.world.gen.feature.template.PlacementSettings;
+import net.minecraft.world.gen.feature.template.BlockIgnoreStructureProcessor;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Mirror;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.coolmodthatsactuallycool.world.dimension.SimonsDimensionDimension;
+import net.mcreator.coolmodthatsactuallycool.block.PurpleGrassBlock;
+import net.mcreator.coolmodthatsactuallycool.CoolModThatsActuallyCoolModElements;
+
+import java.util.Random;
+
+@CoolModThatsActuallyCoolModElements.ModElement.Tag
+public class Huge2GreenMushroomStructure extends CoolModThatsActuallyCoolModElements.ModElement {
+	public Huge2GreenMushroomStructure(CoolModThatsActuallyCoolModElements instance) {
+		super(instance, 28);
+	}
+
+	@Override
+	public void init(FMLCommonSetupEvent event) {
+		Feature<NoFeatureConfig> feature = new Feature<NoFeatureConfig>(NoFeatureConfig::deserialize) {
+			@Override
+			public boolean place(IWorld world, ChunkGenerator generator, Random random, BlockPos pos, NoFeatureConfig config) {
+				int ci = (pos.getX() >> 4) << 4;
+				int ck = (pos.getZ() >> 4) << 4;
+				DimensionType dimensionType = world.getDimension().getType();
+				boolean dimensionCriteria = false;
+				if (dimensionType == SimonsDimensionDimension.type)
+					dimensionCriteria = true;
+				if (!dimensionCriteria)
+					return false;
+				if ((random.nextInt(1000000) + 1) <= 10000) {
+					int count = random.nextInt(4) + 1;
+					for (int a = 0; a < count; a++) {
+						int i = ci + random.nextInt(16);
+						int k = ck + random.nextInt(16);
+						int j = world.getHeight(Heightmap.Type.WORLD_SURFACE_WG, i, k);
+						j = Math.abs(random.nextInt(Math.max(1, j)) - 24);
+						BlockState blockAt = world.getBlockState(new BlockPos(i, j, k));
+						boolean blockCriteria = false;
+						if (blockAt.getBlock() == PurpleGrassBlock.block.getDefaultState().getBlock())
+							blockCriteria = true;
+						if (!blockCriteria)
+							continue;
+						Rotation rotation = Rotation.values()[random.nextInt(3)];
+						Mirror mirror = Mirror.values()[random.nextInt(2)];
+						BlockPos spawnTo = new BlockPos(i, j + 0, k);
+						int x = spawnTo.getX();
+						int y = spawnTo.getY();
+						int z = spawnTo.getZ();
+						Template template = ((ServerWorld) world.getWorld()).getSaveHandler().getStructureTemplateManager()
+								.getTemplateDefaulted(new ResourceLocation("cool_mod_thats_actually_cool", "huge2greenmushroom"));
+						if (template == null)
+							return false;
+						template.addBlocksToWorld(world, spawnTo, new PlacementSettings().setRotation(rotation).setRandom(random).setMirror(mirror)
+								.addProcessor(BlockIgnoreStructureProcessor.AIR_AND_STRUCTURE_BLOCK).setChunk(null).setIgnoreEntities(false));
+					}
+				}
+				return true;
+			}
+		};
+		for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
+			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_STRUCTURES, feature.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG)
+					.withPlacement(Placement.NOPE.configure(IPlacementConfig.NO_PLACEMENT_CONFIG)));
+		}
+	}
+}
